31adb2a3c0973c026b8350d7f9adeb553d1b304b
santinomartinezmerli <45015381@comunidadort.edu.ar>
2026-06-10 00:37:16 -0300

    setup inicial sharebuy

---
+++ sharebuy/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-50">
+      <h1 className="text-4xl font-semibold text-green-600">ShareBuy</h1>
+    </div>
+  )
+}
+
+export default App
